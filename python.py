0f32202bc9f2217b47b5ed92fa99748b38e4308c
funbek <web_tec@mail.ru>
2014-01-26 23:06:23 +0300

    for Pavlusha

--- python.py
+++ python.py
@@ -1,2 +1,79 @@
-a = 'try to use new branch on git'
-print(a)
+# age = 26
+# name = "Denis"
+# print('hight {0} - {1} year'.format(name, age))
+
+
+# secondName = 'Radchenko'
+# print('my secondname is {0}'.format(secondName))
+
+# print('{0:.2}'.format(1/3))
+
+# print('{0:_^11}'.format('hello'))
+
+
+
+# a = 2
+# hey = 1
+
+# if a == 3:
+# 	hey = 21
+# 	print(hey)
+# elif a == 2:
+# 	hey *= 3
+# 	print(hey)
+# else:
+# 	a *= 4
+# 	print(a)
+# print("finish")
+
+# a = 1
+# b = 5
+# c = True
+
+# while c:
+# 	if b == a:
+# 		print('yes, well done b is', b)
+# 		c = False
+# 	elif b > a:
+# 		print( a,'and')
+# 		a += 1
+# else:
+# 	print('somthing else')
+
+
+
+
+# for i in range(1,5):
+# 	print(i)
+# else:
+# 	print('loop for end')
+
+
+
+# def sayHello():
+# 	a = 'Hello, World'
+# 	s = len(a)
+# 	print(a,'length is:', s)
+
+# sayHello()
+
+
+# def printMax(a, b):
+# 	if a < b:
+# 		print('a is less b')
+# 	elif a > b:
+# 		print('a is more b')
+# 	else:
+# 		print('a is equal b')
+
+# printMax(3, 3)
+
+
+
+a = 'somthing text'
+s = len(a)
+
+def sm():
+	print(a, ':', s)
+
+sm()
